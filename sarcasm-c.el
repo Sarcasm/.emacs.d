3332e8ddf29a8b20f69a959de98f1011ecc6d2a5
Guillaume Papin <guillaume.papin@epitech.eu>
2011-04-02 16:34:12 +0200

    Enable semantic completion for structure only when `auto-complete-mode' exist.

--- sarcasm-c.el
+++ sarcasm-c.el
@@ -17,26 +17,29 @@
 (add-hook 'c-mode-hook
 	  '(lambda ()
 	     (semantic-mode)
-	     ;; ac-omni-completion-sources is made buffer local so
-	     ;; you need to add it to a mode hook to activate on
-	     ;; whatever buffer you want to use it with.  This
-	     ;; example uses C mode (as you probably surmised).
-	     ;; auto-complete.el expects ac-omni-completion-sources to be
-	     ;; a list of cons cells where each cell's car is a regex
-	     ;; that describes the syntactical bits you want AutoComplete
-	     ;; to be aware of. The cdr of each cell is the source that will
-	     ;; supply the completion data.  The following tells autocomplete
-	     ;; to begin completion when you type in a . or a ->
-	     (add-to-list 'ac-omni-completion-sources
-			  (cons "\\." '(ac-source-semantic)))
-	     (add-to-list 'ac-omni-completion-sources
-			  (cons "->" '(ac-source-semantic)))
-	     ;; ac-sources was also made buffer local in new versions of
-	     ;; autocomplete.  In my case, I want AutoComplete to use
-	     ;; semantic and yasnippet (order matters, if reversed snippets
-	     ;; will appear before semantic tag completions).
-	     ;; (setq ac-sources '(ac-source-semantic ac-source-yasnippet))
-	     (setq ac-sources (append '(ac-source-semantic) ac-sources))
+
+             (when (boundp 'auto-complete-mode)
+               ;; ac-omni-completion-sources is made buffer local so
+               ;; you need to add it to a mode hook to activate on
+               ;; whatever buffer you want to use it with.  This
+               ;; example uses C mode (as you probably surmised).
+               ;; auto-complete.el expects ac-omni-completion-sources to be
+               ;; a list of cons cells where each cell's car is a regex
+               ;; that describes the syntactical bits you want AutoComplete
+               ;; to be aware of. The cdr of each cell is the source that will
+               ;; supply the completion data.  The following tells autocomplete
+               ;; to begin completion when you type in a . or a ->
+               (add-to-list 'ac-omni-completion-sources
+                            (cons "\\." '(ac-source-semantic)))
+               (add-to-list 'ac-omni-completion-sources
+                            (cons "->" '(ac-source-semantic)))
+               ;; ac-sources was also made buffer local in new versions of
+               ;; autocomplete.  In my case, I want AutoComplete to use
+               ;; semantic and yasnippet (order matters, if reversed snippets
+               ;; will appear before semantic tag completions).
+               ;; (setq ac-sources '(ac-source-semantic ac-source-yasnippet))
+               (setq ac-sources (append '(ac-source-semantic) ac-sources))
+               )
 	     ))
 
 (provide 'sarcasm-c)
