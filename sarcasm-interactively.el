9df3455db4e1ebc80aeed7c180510ccfc492e042
Guillaume Papin <guillaume.papin@epitech.eu>
2011-04-11 09:36:22 +0200

    Start interactive commands customisation.

---
+++ sarcasm-interactively.el
@@ -0,0 +1,24 @@
+;; Minibuffer completion and other interactive commands -- Guillaume Papin
+;; usage:
+;; (require 'sarcasm-interactively)
+
+(iswitchb-mode 1)
+
+;; Iswitchb with Arrow-keys
+(defun iswitchb-local-keys ()
+  (mapc (lambda (K)
+	  (let* ((key (car K)) (fun (cdr K)))
+	    (define-key iswitchb-mode-map (edmacro-parse-keys key) fun)))
+	'(("<right>" . iswitchb-next-match)
+	  ("<left>"  . iswitchb-prev-match)
+	  ;; ("<up>"	. ignore	     )
+	  ;; ("<down>"	. ignore	     )
+	  )))
+(add-hook 'iswitchb-define-mode-map-hook 'iswitchb-local-keys)
+
+;; ;; Different M-x completion
+;; (icomplete-mode 1)
+;; (setq icomplete-prospects-height 1) ;; don't spam minibuffer
+;; ;; (require 'icomplete+) ;; autoload with ELPA I think
+
+(provide 'sarcasm-interactively)
