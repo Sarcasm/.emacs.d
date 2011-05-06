3392c7e6df89b8e4459e696d286f13c31638c0cd
Guillaume Papin <guillaume.papin@epitech.eu>
2011-05-06 11:49:50 +0200

    Add the function `c++-open-decl-or-def-other-window' and map this function to a key.

--- sarcasm-elisp/sarcasm-c++.el
+++ sarcasm-elisp/sarcasm-c++.el
@@ -28,7 +28,33 @@
             (subword-mode)              ;C-c C-w to toggle
             (define-key c++-mode-map (kbd "M-TAB") 'ac-complete-clang)
 
+            (define-key c++-mode-map (kbd "C-c t") 'c++-open-decl-or-def-other-window)
             ;; (setq ac-sources (append '(ac-source-clang) ac-sources))
           ))
+
+(defun c++-open-decl-or-def-other-window ()
+  "Open the declaration (Class.hh) or the definition (Class.cpp)
+of the current file in other window.
+
+Create the file if it doesn't exist.
+
+File.hh => File.cpp
+File.cpp => File.hh"
+  (interactive)
+  (let ((file buffer-file-name))
+    (if (not file)
+        (message "Coulnd't retrieve buffer filename.")
+      (let ((ext (file-name-extension file)))
+        (cond
+         ((string= ext "hh")
+          (find-file-other-window (concat (file-name-sans-extension file) ".cpp")))
+         ((string= ext "cpp")
+          (find-file-other-window (concat (file-name-sans-extension file) ".hh")))
+         (t (message "Invalid filename, extension .hh or .cpp expected."))
+         )
+        )
+      )
+    )
+  )
 
 (provide 'sarcasm-c++)
