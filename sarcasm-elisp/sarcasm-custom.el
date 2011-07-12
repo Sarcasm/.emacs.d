1f47a48ccafc6c7d678bb8411a34b18a9aa92af1
Guillaume Papin <guillaume.papin@epitech.eu>
2011-07-12 09:50:15 +0200

    minor changes in custom file.

--- sarcasm-elisp/sarcasm-custom.el
+++ sarcasm-elisp/sarcasm-custom.el
@@ -11,10 +11,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(c-doc-comment-style (quote ((java-mode . javadoc) (pike-mode . autodoc))))
- '(custom-safe-themes (quote ("feb707bbfec683c596a4ef674c2e2eeb8bf87d3e" "cda71a1145d4f3c3a3d17fcfbfac3077ee561861" "af71844a5f0ad8a150999eb475688d3ed724eb87" "43dc57f73e4547afa94d6b89224008b5b7c713bb" "698361f81d474f433dda381f5eb637cf40e015c0" "234dcb00b7d85b90cb7752254f7226dce7ba69e2" "f2a53fd7b4c967c6c1f5a755c325fc859cc8ea74" default)))
+ '(custom-safe-themes (quote ("cda71a1145d4f3c3a3d17fcfbfac3077ee561861" "f2a53fd7b4c967c6c1f5a755c325fc859cc8ea74" default)))
  '(el-get-standard-packages (quote ("cedet" "el-get" "xcscope" "xcscope+" "rainbow-mode" "flymake-lua" "htmlize" "folding" "rinari" "haml-mode" "sass-mode" "yaml-mode" "flymake-ruby" "magit" "fringe-helper" "lua-mode" "yari" "test-case-mode" "autopair" "doxymacs" "filladapt" "rhtml-mode" "smex" "iedit" "zencoding-mode" "offlineimap" "buffer-move" "switch-window" "yasnippet" "autocomplete" "auto-complete-extension" "auto-complete-clang" "ac-slime")))
  '(garak-alert-methods (quote (:notify)))
- '(safe-local-variable-values (quote ((ac-clang-flags "-I./includes" "-I../includes" "-I." "-I..") (ac-clang-flags "-I/usr/include/qt4/QtCore" "-I/usr/include/qt4/QtGui" "-I/usr/include/qt4" "-I/usr/include/qt4/QtWebKit" "-I." "-I.." "-I../libvmime-0.9.1") (ac-clang-flags "-I." "-I../includes" "-I./arguments") (ruby-compilation-executable . "ruby") (ruby-compilation-executable . "ruby1.8") (ruby-compilation-executable . "ruby1.9") (ruby-compilation-executable . "rbx") (ruby-compilation-executable . "jruby")))))
+ '(garak-alert-when (quote (:new :hidden)))
+ '(safe-local-variable-values (quote ((ac-clang-flags "-I./includes" "-I../includes" "-I." "-I..") (ruby-compilation-executable . "ruby") (ruby-compilation-executable . "ruby1.8") (ruby-compilation-executable . "ruby1.9") (ruby-compilation-executable . "rbx") (ruby-compilation-executable . "jruby")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
