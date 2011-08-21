526cdb9b0570fea578bef008a554b47a5c859b19
Guillaume Papin <guillaume.papin@epitech.eu>
2011-08-21 19:36:39 +0200

    Try to clean this file (for future !).

--- sarcasm-elisp/sarcasm-custom.el
+++ sarcasm-elisp/sarcasm-custom.el
@@ -4,6 +4,14 @@
 ;;       (concat (file-name-as-directory *sarcasm-load-path*)
 ;;               "sarcasm-custom.el"))
 ;; (load custom-file)
+;;
+;; note: the file in is initial state is good, changes made by the
+;; time for safe variable values for example are not interesting to
+;; commit, so to ignore local changes to the file the following
+;; command need to be entered:
+;;
+;;      git update-index --assume-unchanged /path/to/sarcasm-custom.el
+;;
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -11,9 +19,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(c-doc-comment-style (quote ((java-mode . javadoc) (pike-mode . autodoc))))
- '(custom-safe-themes (quote ("c4313ab2ba95b9a04e3052e480105ae6716d7811" "2a9026dfa81109bbc41ee020977623d79e3108ab" "4cb88aa5ff95914b5c768bea88331a753448426f" "76bb2804f786d63060a3ee7e0426e688fc6f184a" "feb707bbfec683c596a4ef674c2e2eeb8bf87d3e" "cda71a1145d4f3c3a3d17fcfbfac3077ee561861" "af71844a5f0ad8a150999eb475688d3ed724eb87" "43dc57f73e4547afa94d6b89224008b5b7c713bb" "698361f81d474f433dda381f5eb637cf40e015c0" "234dcb00b7d85b90cb7752254f7226dce7ba69e2" "f2a53fd7b4c967c6c1f5a755c325fc859cc8ea74" default)))
- '(garak-alert-methods (quote (:notify)))
- '(safe-local-variable-values (quote ((ac-clang-flags "-I.." "-I../utils" "-I../utils/SimpleJSON/src") (ac-clang-flags "-I.." "-I../utils" "-I../utils/SimpleJSON") (ac-clang-flags "-I.." "-I../utils" "-I../SimpleJSON") (sarcasm-sub-project-name . "server") (sarcasm-project-name . "cc-enhanced") (ac-clang-flags "-I./includes" "-I../includes" "-I." "-I..") (ac-clang-flags "-I/usr/include/qt4/QtCore" "-I/usr/include/qt4/QtGui" "-I/usr/include/qt4" "-I/usr/include/qt4/QtWebKit" "-I." "-I.." "-I../libvmime-0.9.1") (ac-clang-flags "-I." "-I../includes" "-I./arguments") (ruby-compilation-executable . "ruby") (ruby-compilation-executable . "ruby1.8") (ruby-compilation-executable . "ruby1.9") (ruby-compilation-executable . "rbx") (ruby-compilation-executable . "jruby")))))
+ '(garak-alert-methods (quote (:notify))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
