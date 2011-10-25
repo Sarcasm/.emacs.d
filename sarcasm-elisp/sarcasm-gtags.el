5e9b5429f5b27e595a50c7be98880d4ab3975bcd
Guillaume Papin <guillaume.papin@epitech.eu>
2011-10-25 22:03:42 +0200

    Trying to make gtags more compatible with Arch Linux and Debian.

--- sarcasm-elisp/sarcasm-gtags.el
+++ sarcasm-elisp/sarcasm-gtags.el
@@ -8,38 +8,46 @@
 ;; http://cvs.savannah.gnu.org/viewvc/global/gtags.el?root=global&view=markup
 ;; (add-to-list 'load-path "~/.emacs.d/global/")
 ;; (autoload 'gtags-mode "gtags" "" t)
-(load-file "/usr/local/share/gtags/gtags.el")
+(defvar sarcasm-have-gtag nil)
 
-;; Easy access to tags
-(define-key gtags-mode-map (kbd "C-.") 'gtags-find-tag-from-here-other-window)
-(define-key gtags-mode-map (kbd "M-.") 'gtags-find-tag-from-here)
+(cond
+ ((file-exists-p "/usr/local/share/gtags/gtags.el") ;debian
+  (setq sarcasm-have-gtag t)
+  (load-file "/usr/local/share/gtags/gtags.el"))
+ ((file-exists-p "/usr/share/gtags/gtags.el") ;archlinux
+  (setq sarcasm-have-gtag t)
+  (load-file "/usr/share/gtags/gtags.el")))
 
-;; Based on gtags-find-tag-from-here
-(defun gtags-find-tag-from-here-other-window ()
-  "Find tags at point and open in other window."
-  (interactive)
-  (let (tagname flag)
-    (setq tagname (gtags-current-token))
-    (when tagname
-      ;; other-window ok...but I want to stay in my current buffer
-      (save-selected-window
-	(gtags-push-context)
-	(gtags-goto-tag tagname "C" t))))
-  )
+(when sarcasm-have-gtag
+  ;; Easy access to tags
+  (define-key gtags-mode-map (kbd "C-.") 'gtags-find-tag-from-here-other-window)
+  (define-key gtags-mode-map (kbd "M-.") 'gtags-find-tag-from-here)
 
-;; Come from the CEDET package
-(require 'pulse)
+  ;; Based on gtags-find-tag-from-here
+  (defun gtags-find-tag-from-here-other-window ()
+    "Find tags at point and open in other window."
+    (interactive)
+    (let (tagname flag)
+      (setq tagname (gtags-current-token))
+      (when tagname
+        ;; other-window ok...but I want to stay in my current buffer
+        (save-selected-window
+          (gtags-push-context)
+          (gtags-goto-tag tagname "C" t)))))
 
-(defadvice gtags-goto-tag (after gtags-goto-tag-my-advice
-activate compile)
-  "After a jump to the tag definition, temporary highlight the
+  ;; Come from the CEDET package
+  (require 'pulse)
+
+  (defadvice gtags-goto-tag (after gtags-goto-tag-my-advice
+                                   activate compile)
+    "After a jump to the tag definition, temporary highlight the
 line under the cursor."
-  (pulse-momentary-highlight-one-line (point)))
+    (pulse-momentary-highlight-one-line (point)))
 
-(defadvice gtags-select-it (after gtags-select-it-my-advice
-activate compile)
-  "After a jump to the tag definition found in Global select
+  (defadvice gtags-select-it (after gtags-select-it-my-advice
+                                    activate compile)
+    "After a jump to the tag definition found in Global select
 buffer, temporary highlight the line under the cursor."
-  (pulse-momentary-highlight-one-line (point)))
+    (pulse-momentary-highlight-one-line (point))))
 
 (provide 'sarcasm-gtags)
