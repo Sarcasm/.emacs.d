fdeead1a5a18701bcc340aedab2a2d82027ed151
Guillaume Papin <guillaume.papin@epitech.eu>
2012-07-04 22:05:21 +0200

    Add a skeleton for Doxygen verbatims (aka "first skeleton").

--- sarcasm-elisp/sarcasm-c++.el
+++ sarcasm-elisp/sarcasm-c++.el
@@ -2,6 +2,12 @@
 ;; usage:
 ;; (require 'sarcasm-c++)
 
+;; '>' - to indent a line
+(define-skeleton skeleton-doxygen-verbatim
+  "Insert a verbatim region"		;doc
+  nil					;prompt
+  "\\verbatim\n" _ "\n\\endverbatim")	; '_' - cursor position
+
 (define-abbrev-table 'c++-mode-abbrev-table
   '(
     ("ALGO"		"#include <algorithm>")
@@ -24,6 +30,7 @@
     ("STR"		"#include <string>")
     ("UNI"		"#include <unistd.h>")
     ("VECTOR"		"#include <vector>")
+    ("VB"               "" skeleton-doxygen-verbatim)
     ))
 
 (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
