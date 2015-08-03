7bb06a9487ab94342772262c04471d522b484164
Guillaume Papin <guillaume.papin@parrot.com>
2015-08-03 10:15:23 +0200

    add function to open file as root

--- sarcasm-elisp/sarcasm-keys.el
+++ sarcasm-elisp/sarcasm-keys.el
@@ -87,4 +87,18 @@
         (sarcasm-unescape-quotes begin end)
       (sarcasm-escape-quotes begin end))))
 
+;; http://emacs-fu.blogspot.fr/2013/03/editing-with-root-privileges-once-more.html
+(defun sarcasm-find-file-as-root ()
+  "Like `ido-find-file, but automatically edit the file with
+root-privileges (using tramp/sudo), if the file is not writable by
+user."
+  (interactive)
+  (let ((file (ido-read-file-name "Edit as root: ")))
+    (unless (file-writable-p file)
+      (setq file (concat "/sudo:root@localhost:" file)))
+    (find-file file)))
+
+;; or some other keybinding...
+(global-set-key (kbd "C-x F") 'sarcasm-find-file-as-root)
+
 (provide 'sarcasm-keys)
