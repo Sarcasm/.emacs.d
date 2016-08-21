c8a7177ee97c6f23814dbf398cd69ad412739b7d
Guillaume Papin <guillaume.papin@parrot.com>
2016-08-21 20:18:24 +0200

    update customizations

--- sarcasm-elisp/sarcasm-custom.el
+++ sarcasm-elisp/sarcasm-custom.el
@@ -17,12 +17,18 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ag-highlight-search t)
  '(ansi-color-names-vector
    ["black" "firebrick" "lime green" "yellow2" "DodgerBlue2" "deep pink" "cornflower blue" "white"])
  '(c-doc-comment-style (quote ((java-mode . javadoc) (pike-mode . autodoc))))
  '(clang-format-executable "clang-format-3.8")
  '(garak-alert-methods (quote (:notify)))
- '(magit-revert-buffers (quote silent))
+ '(irony-cdb-compilation-databases
+   (quote
+    (irony-cdb-clang-complete irony-cdb-libclang sarcasm-irony-cdb-not-found)))
+ '(magit-diff-arguments (quote ("--no-ext-diff" "-M")))
+ '(magit-diff-refine-hunk t)
+ '(magit-revert-buffers (quote silent) t)
  '(menu-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
