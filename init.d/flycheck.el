819c03e2970ed28690078d3a605c56d8cefb9240
Guillaume Papin <guillaume.papin@epitech.eu>
2017-06-06 22:56:03 +0200

    configure display buffer

--- init.d/flycheck.el
+++ init.d/flycheck.el
@@ -1,5 +1,8 @@
 (require 'sarcasm)
 
+(eval-when-compile
+  (require 'rx))
+
 (use-package flycheck
   :ensure t
   :defer 3
@@ -9,3 +12,24 @@
   :config (sarcasm-deffered-global-mode #'global-flycheck-mode
                                         #'flycheck-mode-on-safe
                                         'flycheck-mode))
+
+;; Configure `display-buffer' behaviour for some special buffers.
+;; see http://www.lunaryorn.com/2015/04/29/the-power-of-display-buffer-alist.html
+;; and https://github.com/lunaryorn/.emacs.d/blob/2233f7dc277453b7eaeb447b00d8cb8d72435318/init.el#L420-L439
+(setq display-buffer-alist
+      `(
+        ;; Put REPLs and error lists into the bottom side window
+        (,(rx bos (or "*Flycheck errors*" ; Flycheck error list
+                      "*compilation"      ; Compilation buffers
+                      "*Warnings*"        ; Emacs warnings
+                      "*shell"            ; Shell window
+                      ))
+         (display-buffer-reuse-window
+          display-buffer-in-side-window)
+         (side            . bottom)
+         (reusable-frames . visible)
+         (window-height   . 0.22))
+        ;; Let `display-buffer' reuse visible frames for all buffers.
+        ;; This must be the last entry in `display-buffer-alist',
+        ;; because it overrides any later entry with more specific actions.
+        ("." nil (reusable-frames . visible))))
