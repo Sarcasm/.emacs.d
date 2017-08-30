38cdbf1c68b5b8a26f6ece867ffe684b90758d6d
Guillaume Papin <guillaume.papin@epitech.eu>
2017-08-30 19:47:03 +0200

    company completion for git commit message

--- init.d/completion.el
+++ init.d/completion.el
@@ -1,5 +1,16 @@
 (require 'sarcasm)
 
+(defun sarcasm-company-dabbrev-ignore-except-magit-diff (buffer)
+  (let ((name (buffer-name)))
+    (and (string-match-p "\\`[ *]" name)
+         (not (string-match-p "\\*magit-diff:" name)))))
+
+(defun sarcasm-git-commit-setup-hook ()
+  (setq-local company-dabbrev-code-modes '(text-mode magit-diff-mode))
+  (setq-local company-dabbrev-code-other-buffers 'code)
+  (setq-local company-dabbrev-ignore-buffers
+              #'sarcasm-company-dabbrev-ignore-except-magit-diff))
+
 (use-package company
   :ensure t
   :defer 0
@@ -8,9 +19,18 @@
   :preface (declare-function company-mode-on "ext:company")
   :config
   (setq company-selection-wrap-around t)
+  (setq company-dabbrev-downcase nil)
+  (setq company-dabbrev-ignore-case nil)
   (sarcasm-deffered-global-mode #'global-company-mode
                                 #'company-mode-on
-                                'company-mode))
+                                'company-mode)
+
+  ;; When redacting commit message in magit,
+  ;; with the diff view in the other window,
+  ;; use company-dabbrev to complete words of the other buffer
+  ;; in the commit message buffer.
+  ;; Stolen from https://github.com/company-mode/company-mode/issues/704#issuecomment-325783249
+  (add-hook 'git-commit-setup-hook #'sarcasm-git-commit-setup-hook))
 
 (use-package ivy
   :ensure t
