f191c63aac5129680e814dabefc249c371c99df5
Guillaume Papin <guillaume.papin@epitech.eu>
2022-04-23 15:48:29 +0200

    deleted some ported init.d/ el files

--- init.d/advice.el
+++
@@ -1,3 +0,0 @@
-(defadvice narrow-to-region (after narrow-to-region-unmark activate compile)
-  "Disable selection after `narrow-to-region'."
-  (deactivate-mark))
--- init.d/mode-line.el
+++
@@ -1,7 +0,0 @@
-(use-package moody
-  :ensure t
-  :config
-  (setq x-underline-at-descent-line t)
-  (setq moody-mode-line-height 20)
-  (moody-replace-mode-line-buffer-identification)
-  (moody-replace-vc-mode))
